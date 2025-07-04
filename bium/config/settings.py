f372d691c3a23e73ff82812e3f40d03ac8f3fe89
rrinny <r_r_i_n@naver.com>
2025-07-05 00:49:56 +0900

    ✨ feat: 유언장 API 개발

--- bium/config/settings.py
+++ bium/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'will',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_URL = '/api/users/login/'
