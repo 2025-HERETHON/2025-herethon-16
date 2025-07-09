f4ed386426af63d637d52977c47608ea99736dec
rrinny <r_r_i_n@naver.com>
2025-07-10 00:43:54 +0900

    🐛 fix: URL 패턴 수정

--- bium/users/views.py
+++ bium/users/views.py
@@ -62,6 +62,9 @@ def login_view(request):
 
     if user is not None:
         login(request, user)
-        # return redirect('main')   // 메인 페이지로 이동
+        return redirect('/')
     else:
         return render(request, 'login.html', {"error": "아이디나 비밀번호가 올바르지 않습니다."})
+
+def main_view(request):
+    return render(request, 'main-page.html')
